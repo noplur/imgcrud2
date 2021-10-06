05a51ca28ee6a5c9fc1514ddc6cf40de370be287
noplur <geoduckphoto@gmail.com>
2021-10-05 22:09:43 -0700

    heroku update

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+
+const PORT = process.env.PORT || 5000;
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
@@ -23,4 +25,6 @@ app.use(cors());
 // Route
 app.use("/user", require("./routes/user"));
 
-app.listen(5000, () => console.log("Server is running"));
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
