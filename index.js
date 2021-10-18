649dc57da812c53ae410e7d0b7739ba2b5dfeac9
noplur <geoduckphoto@gmail.com>
2021-10-17 19:57:01 -0700

    test

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-dotenv.config();
 const cors = require("cors");
+
+dotenv.config();
 
 // Connect DB
 mongoose
   .connect(process.env.MONGO_URI, {
-    // useCreateIndex: true,
+    useCreateIndex: true,
     useNewUrlParser: true,
-    // useFindAndModify: false,
+    useFindAndModify: false,
     useUnifiedTopology: true,
   })
   .then(() => console.log("mongoDB is connected"))
